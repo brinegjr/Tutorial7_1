e692e812ba914c641ec784ae8ca6d2918b2abeb1
Jeremy Brinegar <brinegjr@rose-hulman.edu>
2016-04-26 13:59:30 -0400

    PawnMovement "one space forward" fails

---
+++ CucumberTests/PawnMovement.feature
@@ -0,0 +1,11 @@
+﻿Feature: PawnMovement
+	As a player,
+	I want to be able to move my pawn one space forward,
+	or two spaces forward when the pawn has not moved.
+
+@movement @happypath
+Scenario: One space forward
+	Given that I have an empty chessboard
+	And I have a white pawn at a2
+	When I try to move the white pawn from a2 to a3
+	Then the white pawn is placed in a3.
---
+++ CucumberTests/PawnMovementSteps.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class PawnMovementSteps
+    {
+        Chessboard board = null;
+        [Given(@"that I have an empty chessboard")]
+        public void GivenThatIHaveAnEmptyChessboard()
+        {
+            board = new Chessboard();
+        }
+
+        Pawn p = null;
+        [Given(@"I have a white pawn at (.)(.)")]
+        public void GivenIHaveAWhitePawnAtA(char x, int y)
+        {
+            p = new Pawn("white");
+            board.placePiece(p, new Tuple<char, int>(x, y));
+        }
+        
+        [When(@"I try to move the white pawn from (.)(.) to (.)(.)")]
+        public void WhenITryToMoveTheWhitePawnFromAToA(char x, int y, char x2, int y2)
+        {
+            board.movePiece(new Tuple<char, int>(x, y), new Tuple<char, int>(x2, y2));
+        }
+        
+        [Then(@"the white pawn is placed in (.)(.)\.")]
+        public void ThenTheWhitePawnIsPlacedInA_(char x, int y)
+        {
+            Assert.AreEqual(new Tuple<char, int>(x, y), board.getLocation(p));
+        }
+    }
+}
