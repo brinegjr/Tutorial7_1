ba62c71c744488f869a1cf447778b38e7eb57020
Jeremy Brinegar <brinegjr@rose-hulman.edu>
2016-04-28 01:55:17 -0400

    CaptureInPassing fails

---
+++ CucumberTests/CaptureInPassing.feature
@@ -0,0 +1,10 @@
+﻿Feature: CaptureInPassing
+
+@capture @happypath
+Scenario: Capture in passing
+	Given that I have an empty chessboard
+	And I have a white pawn at e5
+	And I have a black pawn at f7
+	When I try to move the black pawn from f7 to f5
+	And I try to move the white pawn from e5 to f6
+	Then the black pawn is captured in passing
---
+++ CucumberTests/CaptureInPassingSteps.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class CaptureInPassingSteps
+    {
+        public PawnMovementSteps p;
+        public PawnCaptureSteps p2;
+
+        public CaptureInPassingSteps(PawnMovementSteps p,
+            PawnCaptureSteps p2)
+        {
+            this.p = p;
+            this.p2 = p2;
+        }
+
+        [Then(@"the black pawn is captured in passing")]
+        public void ThenTheBlackPawnIsCapturedInPassing()
+        {
+            Assert.IsTrue(this.p2.blackPawn.isCaptured());
+        }
+    }
+}
