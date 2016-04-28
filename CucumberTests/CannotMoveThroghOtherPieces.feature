bd337bf3201c03a9bad68bbd152e4d7815665052
Jeremy Brinegar <brinegjr@rose-hulman.edu>
2016-04-27 23:03:50 -0400

    CannotMoveThroughPieces fails

---
+++ CucumberTests/CannotMoveThroghOtherPieces.feature
@@ -0,0 +1,11 @@
+﻿Feature: CannotMoveThroghOtherPieces
+	As a player, I want my to not be able to
+	move through other pieces.
+
+@movement @happypath
+Scenario: Cannot move through other pieces
+	Given that I have an empty chessboard
+	And I have a black pawn at e5
+	And I have a white pawn at e4
+	When I try to move the white pawn from e4 to e5
+	Then the white pawn does not move
---
+++ CucumberTests/CannotMoveThroghOtherPiecesSteps.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class CannotMoveThroghOtherPiecesSteps
+    {
+        public PawnMovementSteps steps;
+        public PawnCaptureSteps capt;
+
+        public CannotMoveThroghOtherPiecesSteps(PawnMovementSteps steps,
+            PawnCaptureSteps capt)
+        {
+            this.steps = steps;
+            this.capt = capt;
+        }
+
+        [Then(@"the white pawn does not move")]
+        public void ThenTheWhitePawnDoesNotMove()
+        {
+            Assert.AreEqual(new Tuple<char, int>('e', 4),
+                this.steps.board.getLocation(this.steps.p));
+        }
+    }
+}
