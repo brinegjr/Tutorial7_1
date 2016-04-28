1d91c5787865a12c17c795a1b2078b2b762ccbbb
Jeremy Brinegar <brinegjr@rose-hulman.edu>
2016-04-28 01:22:50 -0400

    CaptureOnlyWhenDiagonalSteps fails

---
+++ CucumberTests/CaptureOnlyWhenDiagonalSteps.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class CaptureOnlyWhenDiagonalSteps
+    {
+        public PawnMovementSteps p;
+        public PawnCaptureSteps p2;
+
+        public CaptureOnlyWhenDiagonalSteps(PawnMovementSteps p,
+            PawnCaptureSteps p2)
+        {
+            this.p = p;
+            this.p2 = p2;
+        }
+
+        [When(@"the white pawn is not captured")]
+        public void WhenTheWhitePawnIsNotCaptured()
+        {
+            Assert.IsFalse(this.p.p.isCaptured());
+        }
+    }
+}
