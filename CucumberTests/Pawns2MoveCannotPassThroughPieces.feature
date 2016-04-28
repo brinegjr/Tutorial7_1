ccf6a81b1eb01334f1c5aed7fb5bb0409deef8b2
Jeremy Brinegar <brinegjr@rose-hulman.edu>
2016-04-27 23:16:07 -0400

    2MoveThroughPieces fails

---
+++ CucumberTests/Pawns2MoveCannotPassThroughPieces.feature
@@ -0,0 +1,9 @@
+﻿Feature: Pawns initial 2 move cannot pass through other pieces
+
+@movement @happypath
+Scenario: 2MoveThroughPieces
+	Given that I have an empty chessboard
+	And I have a black pawn at e3
+	And I have a white pawn at e2
+	When I try to move the white pawn from e2 to e4
+	Then the white pawn does not move again
---
+++ CucumberTests/PawnsInitial2MoveCannotPassThroughOtherPiecesSteps.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace CucumberTests
+{
+    [Binding]
+    public class PawnsInitial2MoveCannotPassThroughOtherPiecesSteps
+    {
+        public PawnMovementSteps p;
+        public PawnCaptureSteps p2;
+
+        public PawnsInitial2MoveCannotPassThroughOtherPiecesSteps(PawnMovementSteps p,
+            PawnCaptureSteps p2)
+        {
+            this.p = p;
+            this.p2 = p2;
+        }
+
+        [Then(@"the white pawn does not move again")]
+        public void ThenTheWhitePawnDoesNotMoveAgain()
+        {
+            Assert.AreEqual(new Tuple<char, int>('e', 2),
+                this.p.board.getLocation(this.p.p));
+        }
+    }
+}
